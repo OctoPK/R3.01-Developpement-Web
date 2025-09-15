6593459146d511e3ef87b0378a01954a7438eed3
OctoPK <armand@biguey.com>
2025-09-15 17:29:49 +0200

    add ex 7 and 8

---
+++ TD/TD1/ex7.js
@@ -0,0 +1,3 @@
+let min = 3;
+total = 2;
+console.log(total >= min ? "Livraison gratuite !" : "Frais de livraison : 20 euros");
---
+++ TD/TD1/ex8.js
@@ -0,0 +1,3 @@
+nb = "0010110";
+conv = Number.parseInt(nb);
+console.log("nb binaire string : " + nb + " / conversion : " + conv);
