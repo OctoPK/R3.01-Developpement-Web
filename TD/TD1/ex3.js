cddae9e618cd78fb7efc081977b681c0b37dfca0
OctoPK <armand@biguey.com>
2025-09-15 17:13:56 +0200

    add ex 3 and 4

---
+++ TD/TD1/ex3.js
@@ -0,0 +1,4 @@
+prenom = "Ethan";
+nom = "Coombes";
+s = prenom + " " + nom;
+console.log(s);
---
+++ TD/TD1/ex4.js
@@ -0,0 +1,4 @@
+HT = 200;
+let TVA = 20;
+TTC = HT * (1 + TVA / 100);
+console.log("HT : " + HT + " / TVA : " + TVA + " / TTC : " + TTC);
