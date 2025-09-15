85f0c9f957b75966099b8a206fa95a368bd22953
OctoPK <armand@biguey.com>
2025-09-15 17:25:40 +0200

    add ex 5 and 6

---
+++ TD/TD1/ex5.js
@@ -0,0 +1,3 @@
+if ((4 >= 6 || "herbe" != "verte") && !(12 * 2 == 144 && true)) console.log(true);
+else console.log(false);
+console.log("test");
---
+++ TD/TD1/ex6.js
@@ -0,0 +1,4 @@
+poid = 132;
+taille = 1.3;
+IMC = poid / taille ** 2;
+console.log(Number.parseFloat(IMC).toFixed(2));
